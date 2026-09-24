2e5e242257d70a253d366e9e5983aeec3b5b1a2f
Gabriel <gabriel.n.dantes@gmail.com>
2026-09-23 19:23:11 -0700

    Reverted the old push. Seperated the pydantic models from 'main.py' and setup them up in 'schemas.py'. In doing so, modified 'schemas' to 'schemas.py' to make the import into 'main.py' work.

--- app/main.py
+++ app/main.py
@@ -1,13 +1,22 @@
 from fastapi import FastAPI
+from app.schemas import Restaurant
 
 app = FastAPI()
 
+# temporary test data for restaurants
+restaurants = [
+    Restaurant(id=1, name="test restaurant 1", cuisine="Italian", description="test desc 1", rating=4.5),
+    Restaurant(id=2, name="test restaurant 2", cuisine="Japanese", description="test desc 2", rating=4.7)
+]
 
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
 
+@app.get("/restaurants/", response_model=list[Restaurant])
+def get_restaurants():
+    return restaurants
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: str | None = None):
-    return {"item_id": item_id, "q": q}
+@app.get("/restaurants/{id}", response_model=Restaurant)
+def read_restaurant(id: int):
+    return restaurants[id]
---
+++ app/schemas.py
@@ -0,0 +1,8 @@
+from pydantic import BaseModel
+
+class Restaurant(BaseModel):
+    id: int
+    name: str
+    cuisine: str | None = None
+    description: str | None = None
+    rating: float | None = None
